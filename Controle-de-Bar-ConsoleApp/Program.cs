73c875e6521663ddcd66d026b15d3067fc336cf4
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-06-19 12:43:09 -0300

    Add project files.

---
+++ Controle-de-Bar-ConsoleApp/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Controle_de_Bar_ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
