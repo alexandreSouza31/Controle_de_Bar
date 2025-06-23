c95da5a6c490badfd6956da9469b8384b7ca574f
alexandreSouza31 <alexandre31_m@hotmail.com>
2025-06-23 17:22:17 -0300

    implementará o módulo de produtos

--- Controle-de-Bar-ConsoleApp/ModuloMesa/TelaMesa.cs
+++ Controle-de-Bar-ConsoleApp/ModuloMesa/TelaMesa.cs
@@ -18,7 +18,7 @@ public class TelaMesa : TelaBase<Mesa>, ITela
         Console.WriteLine();
 
         Console.WriteLine(
-            "{0, -10} | {1, -30} | {2, -20} | {3, -30}",
+            "{0, -10} | {1, -10} | {2, -10} | {3, -10}",
             "Id", "Número", "Capacidade", "Status"
         );
 
@@ -34,7 +34,7 @@ public class TelaMesa : TelaBase<Mesa>, ITela
             string statusMesa = m.EstaOcupada ? "Ocupada" : "Disponível";
 
             Console.WriteLine(
-              "{0, -10} | {1, -20} | {2, -20} | {3, -30}",
+              "{0, -10} | {1, -10} | {2, -10} | {3, -10}",
                 m.Id, m.Numero, m.Capacidade, statusMesa
             );
         }
